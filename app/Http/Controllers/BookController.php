da3903185469035e3f880595c92384348a81da88
Iago Oliveira <ioads@outlook.com>
2024-09-03 08:38:58 -0300

    feat: remove create method

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,14 +26,6 @@ class BookController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreBookRequest $request)
